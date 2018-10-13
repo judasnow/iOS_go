3b261b5e837fece80e5fb5b0d595cb8c5e41dcb6
judasnow <judasnow@gmail.com>
2018-10-13 20:48:40 +0800

    add quiz

---
+++ quiz/quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quiz
+//
+//  Created by judasnow on 2018/10/13.
+//  Copyright © 2018 judasnow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quiz/quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quiz
+//
+//  Created by judasnow on 2018/10/13.
+//  Copyright © 2018 judasnow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
